442f726de92084c4f1b49347c1ea94a94949b3ee
Sho Hashimoto <hashimoto@shokai.org>
2017-09-13 17:11:16 +0900

    rewriting tests with express callback style

--- test/express-style.js
+++ test/express-style.js
@@ -1,28 +1,55 @@
 /* eslint-env mocha */
 
 const {assert} = require('chai')
-const {delay} = require('./helper')
 const combineMiddlewares = require('../')
 
 describe('express style middleware', function () {
-  it('combines middlewares', function () {
-    const m1 = (results, next) => {
-      results.push('m1 called')
-      next()
-    }
-    const m2 = (results, next) => {
-      results.push('m2 called')
-      next()
-    }
-    const m3 = (results, next) => {
-      results.push('m3 called')
-      next()
-    }
+  const auth = (req, res, next) => {
+    if (!req.user) return res.error('no-user')
+    next()
+  }
+  const validate = (req, res, next) => {
+    if (!/^[a-zA-Z]+$/.test(req.data)) return res.error('no-alphabet')
+    next()
+  }
+  const upcase = (req, res, next) => {
+    res.end(req.data.toUpperCase())
+  }
+  const combined = combineMiddlewares(auth, validate, upcase)
 
-    const combined = combineMiddlewares(m1, m2, m3)
-    const results = []
-    combined(results)
-    assert.deepEqual(results, ['m1 called', 'm2 called', 'm3 called'])
+  it('reject if not user', function (done) {
+    combined({data: 'hello'}, {
+      error: (err) => {
+        assert.equal(err, 'no-user')
+        done()
+      },
+      end: (end) => {
+        done('auth middleware is not working')
+      }
+    })
+  })
+
+  it('reject if not alphabet', function (done) {
+    combined({user: 'shokai', data: '123'}, {
+      error: (err) => {
+        assert.equal(err, 'no-alphabet')
+        done()
+      },
+      end: (end) => {
+        done('validate middleware is not working')
+      }})
+  })
+
+  it('upcase alphabets', function (done) {
+    combined({user: 'shokai', data: 'hello'}, {
+      error: (err) => {
+        done('upcase middleware is not working')
+      },
+      end: (end) => {
+        assert.equal(end, 'HELLO')
+        done()
+      }
+    })
   })
 
   it('combine multiple argument middlewares', function () {
